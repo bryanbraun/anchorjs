a472899fd58c34c8ce138b23c844e4c27a1aeadc
Bryan Braun <bbraun7@gmail.com>
2015-04-29 23:22:01 -0400

    Add ten more automated tests for verifying AnchorJS functionality.

--- test/spec/AnchorSpec.js
+++ test/spec/AnchorSpec.js
@@ -21,6 +21,92 @@ describe("AnchorJS", function() {
     expect(anchorLink).not.toBe(null);
   });
 
+  it("should have default options (right placement, hover visiblity, & link icon)", function() {
+    var anchorLink,
+        hasClass,
+        opacity,
+        icon;
+    anchors.add();
+    anchorLink = document.getElementsByTagName('h1')[0].lastChild;
+    hasClass = anchorLink.classList.contains('anchorjs-link');
+    opacity = anchorLink.style.opacity;
+    icon = document.querySelector('.anchorjs-icon').getAttribute('data-anchorjs-icon');
+
+    expect(hasClass).toBe(true);
+    expect(opacity).not.toEqual('1');
+    expect(icon).toEqual('');
+  });
+
+  it("should set baseline styles in the document head", function() {
+    var hasClass;
+    anchors.add();
+    // We query for the first style tag because we are testing both that it's
+    // there and that it's overridable by other styles later in the cascade.
+    hasClass = document.head.getElementsByTagName('style')[0].classList.contains('anchorjs');
+    expect(hasClass).toBe(true);
+  });
+
+  it("allows you to instantiate a new AnchorJS object that behaves like the default one.", function() {
+    var anchorObj,
+        anchorLink;
+    anchorObj = new AnchorJS();
+    anchorObj.add();
+    anchorLink = document.querySelector('h1 > .anchorjs-link');
+    expect(anchorLink).not.toBe(null);
+  });
+
+  it("ensures new AnchorJS instances don't add multiple baseline style tags.", function() {
+    var anchorObj,
+        anchorLink;
+    anchors.add();
+    anchorObj = new AnchorJS();
+    anchorObj.add();
+    styleNodes = document.head.querySelectorAll('.anchorjs');
+    expect(styleNodes.length).toEqual(1);
+  });
+
+  it("doesn't destroy default options when setting an incomplete options object.", function() {
+    var anchorObj,
+        anchorLink,
+        hasClass,
+        opacity,
+        icon;
+    anchors.options = { class: 'test-class' };
+    anchors.add();
+    anchorObj = new AnchorJS({ class: 'test-class' });
+
+    expect(anchors.options.icon).toEqual('&#xe9cb');
+    expect(anchorObj.options.icon).toEqual('&#xe9cb');
+    expect(anchors.options.visible).toEqual('hover');
+    expect(anchorObj.options.visible).toEqual('hover');
+    expect(anchors.options.placement).toEqual('right');
+    expect(anchorObj.options.placement).toEqual('right');
+  });
+
+  it("can remove anchors, using the .remove() method.", function() {
+    var anchorLinkBefore,
+        anchorLinkAfter;
+    anchors.add();
+    anchorLinkBefore = document.querySelector('h1 > .anchorjs-link');
+    expect(anchorLinkBefore).not.toBe(null);
+
+    anchors.remove('h1');
+    anchorLinkAfter = document.querySelector('h1 > .anchorjs-link');
+    expect(anchorLinkAfter).toBe(null);
+  });
+
+  it("can chain methods.", function() {
+    var anchorLinkBefore,
+        anchorLinkAfter;
+    anchors.add('h1').remove('h1');
+    anchorLinkBefore = document.querySelector('h1 > .anchorjs-link');
+    expect(anchorLinkBefore).toBe(null);
+
+    anchors.remove('h1').add('h1');
+    anchorLinkAfter = document.querySelector('h1 > .anchorjs-link');
+    expect(anchorLinkAfter).not.toBe(null);
+  });
+
   it("should create a URL-appropriate ID (and href) for targeted elements without IDs.", function() {
     var href,
         id;
@@ -98,7 +184,7 @@ describe("AnchorJS", function() {
     }).toThrowError("The selector provided to AnchorJS was invalid.");
   });
 
-  it("should place the # icon, when that option is set.", function() {
+  it("should place the ¶ icon, when that option is set.", function() {
     var icon;
     anchors.options.icon = '¶';
     anchors.add('h1');
@@ -106,4 +192,28 @@ describe("AnchorJS", function() {
     expect(icon).toEqual('¶');
   });
 
+  it("should place anchors to the left, when that option is set.", function() {
+    var anchorNode;
+    anchors.options.placement = 'left';
+    anchors.add('h1');
+    anchorNode = document.getElementsByTagName('h1')[0].firstChild;
+    expect(anchorNode.style.float).toEqual('left');
+    expect(anchorNode.style.marginLeft).toEqual('-1.25em');
+  });
+
+  it("should make anchors always visible, when that option is set.", function() {
+    var opacity;
+    anchors.options.visible = 'always';
+    anchors.add('h1');
+    opacity = document.querySelector('.anchorjs-link').style.opacity;
+    expect(opacity).toEqual('1');
+  });
+
+  it("should apply a provided class, when that option is set.", function() {
+    var anchorLink;
+    anchors.options.class = 'test-class';
+    anchors.add('h1');
+    anchorLink = document.querySelector('h1 > .test-class');
+    expect(anchorLink).not.toBe(null);
+  });
 });
