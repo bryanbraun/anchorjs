69679dd903f52368e342ab3eb17918880ec4db70
Bryan Braun <bbraun7@gmail.com>
2016-01-24 00:50:22 -0500

    :white_check_mark: Reorganize tests, and make them more granular.

--- test/spec/AnchorSpec.js
+++ test/spec/AnchorSpec.js
@@ -3,18 +3,15 @@
 
 describe('AnchorJS', function() {
   'use strict';
-  var h,
-      t;
+  var el1;
 
   beforeEach(function() {
-    h = document.createElement('h1');
-    t = document.createTextNode(' ⚡⚡ Don\'t forget: URL fragments should be i18n-friendly, hyphenated, short, and clean.');
-    h.appendChild(t);
-    document.body.appendChild(h);
+    var titleText = ' ⚡⚡ Don\'t forget: URL fragments should be i18n-friendly, hyphenated, short, and clean.';
+    el1 = appendElementToBody('h1', titleText);
   });
 
   afterEach(function() {
-    document.body.removeChild(h);
+    document.body.removeChild(el1);
   });
 
   it('should add an anchor link to an h1 by default', function() {
@@ -24,20 +21,29 @@ describe('AnchorJS', function() {
     expect(anchorLink).not.toBe(null);
   });
 
-  it('should have default options (right placement, hover visiblity, & link icon)', function() {
-    var anchorLink,
-        hasClass,
-        opacity,
-        icon;
+  it('should set the expected default options', function() {
     anchors.add();
-    anchorLink = document.getElementsByTagName('h1')[0].lastChild;
-    hasClass = anchorLink.classList.contains('anchorjs-link');
-    opacity = anchorLink.style.opacity;
-    icon = document.querySelector('.anchorjs-link').getAttribute('data-anchorjs-icon');
 
-    expect(hasClass).toBe(true);
-    expect(opacity).not.toEqual('1');
-    expect(icon).toEqual('\ue9cb');
+    expect(anchors.options.icon).toEqual('\ue9cb');
+    expect(anchors.options.visible).toEqual('hover');
+    expect(anchors.options.placement).toEqual('right');
+    expect(anchors.options.class).toEqual('');
+    expect(anchors.options.truncate).toEqual(64);
+  });
+
+  it('does not destroy default options when setting an incomplete options object', function() {
+    var anchorObj;
+
+    anchors.options = { class: 'test-class' };
+    anchors.add();
+    anchorObj = new AnchorJS({ class: 'test-class' });
+
+    expect(anchors.options.icon).toEqual('\ue9cb');
+    expect(anchorObj.options.icon).toEqual('\ue9cb');
+    expect(anchors.options.visible).toEqual('hover');
+    expect(anchorObj.options.visible).toEqual('hover');
+    expect(anchors.options.placement).toEqual('right');
+    expect(anchorObj.options.placement).toEqual('right');
   });
 
   it('should set baseline styles in the document head', function() {
@@ -49,7 +55,7 @@ describe('AnchorJS', function() {
     expect(hasClass).toBe(true);
   });
 
-  it('allows you to instantiate a new AnchorJS object that behaves like the default one.', function() {
+  it('allows you to instantiate a new AnchorJS object that behaves like the default one', function() {
     var anchorObj,
         anchorLink;
     anchorObj = new AnchorJS();
@@ -66,21 +72,6 @@ describe('AnchorJS', function() {
     anchorObj.add();
     styleNodes = document.head.querySelectorAll('.anchorjs');
     expect(styleNodes.length).toEqual(1);
-  });
-
-  it('does not destroy default options when setting an incomplete options object.', function() {
-    var anchorObj;
-
-    anchors.options = { class: 'test-class' };
-    anchors.add();
-    anchorObj = new AnchorJS({ class: 'test-class' });
-
-    expect(anchors.options.icon).toEqual('\ue9cb');
-    expect(anchorObj.options.icon).toEqual('\ue9cb');
-    expect(anchors.options.visible).toEqual('hover');
-    expect(anchorObj.options.visible).toEqual('hover');
-    expect(anchors.options.placement).toEqual('right');
-    expect(anchorObj.options.placement).toEqual('right');
   });
 
   it('can remove anchors, using the .remove() method.', function() {
@@ -129,36 +120,21 @@ describe('AnchorJS', function() {
   });
 
   it('should increment new IDs if multiple IDs are found on a page.', function() {
-    var h1,
-        t1,
+    var el1 = appendElementToBody('h2', 'Example Title'),
+        el2 = appendElementToBody('h2', 'Example Title'),
+        el3 = appendElementToBody('h2', 'Example Title'),
         id1,
-        href1,
-        h2,
-        t2,
         id2,
-        href2,
-        h3,
-        t3,
         id3,
+        href1,
+        href2,
         href3,
         tags,
-        links,
-        i;
-    h1 = document.createElement('h2');
-    t1 = document.createTextNode('Example Title');
-    h1.appendChild(t1);
-    h2 = document.createElement('h2');
-    t2 = document.createTextNode('Example Title');
-    h2.appendChild(t2);
-    h3 = document.createElement('h2');
-    t3 = document.createTextNode('Example Title');
-    h3.appendChild(t3);
-    document.body.appendChild(h1);
-    document.body.appendChild(h2);
-    document.body.appendChild(h3);
+        links;
+
     anchors.add('h2');
     tags = document.getElementsByTagName('h2');
-    links = document.querySelectorAll('.anchorjs-link');
+    links = document.querySelectorAll('h2 > .anchorjs-link');
     id1 = tags[0].getAttribute('id');
     href1 = links[0].getAttribute('href');
     id2 = tags[1].getAttribute('id');
@@ -173,9 +149,9 @@ describe('AnchorJS', function() {
     expect(id3).toEqual('example-title-2');
     expect(href3).toEqual('#example-title-2');
 
-    document.body.removeChild(h1);
-    document.body.removeChild(h2);
-    document.body.removeChild(h3);
+    document.body.removeChild(el1);
+    document.body.removeChild(el2);
+    document.body.removeChild(el3);
   });
 
   it('should throw an error if an inappropriate selector is provided.', function() {
@@ -184,125 +160,173 @@ describe('AnchorJS', function() {
     }).toThrowError('The selector provided to AnchorJS was invalid.');
   });
 
-  it('should place the ¶ icon, when that option is set.', function() {
-    var icon;
-    anchors.options.icon = '¶';
-    anchors.add('h1');
-    icon = document.querySelector('.anchorjs-link').getAttribute('data-anchorjs-icon');
-    expect(icon).toEqual('¶');
-  });
+  describe('urlify', function() {
+    it('preserves unicode characters', function() {
+      var text1Before = 'Заголовок, содержащий 29 не-ASCII символов',
+          text1After = 'заголовок-содержащий-29-не-ascii-символов',
+          text2Before = '船や航海を連想させるものとして、シンボル的に用いられることも多い。',
+          text2After = '船や航海を連想させるものとして、シンボル的に用いられることも多い。',
+          text3Before = 'അടിത്തട്ടിലെ മണ്ണിൽ ആഴ്ന്നിറങ്ങുക, ഭാരത്താൽ താഴ്ന്നു കിടക്കുക, കപ്പലിന്റെ ഗുരുത്വകേന്ദ്രവും',
+          text3After = 'അടിത്തട്ടിലെ-മണ്ണിൽ-ആഴ്ന്നിറങ്ങുക-ഭാരത്താൽ-താഴ്ന്നു-കിടക്കുക-കപ്',
+          text4Before = 'Use ⚓ and 👪 all over the 🌐 can 🔗 inside your webpages.',
+          text4After = 'use-⚓-and-👪-all-over-the-🌐-can-🔗-inside-your-webpages';
 
-  it('should place anchors to the left, when that option is set.', function() {
-    var anchorNode;
-    anchors.options.placement = 'left';
-    anchors.add('h1');
-    anchorNode = document.getElementsByTagName('h1')[0].firstChild;
-    expect(anchorNode.style.position).toEqual('absolute');
-    expect(anchorNode.style.marginLeft).toEqual('-1em');
-  });
-
-  it('should make anchors always visible, when that option is set.', function() {
-    var opacity;
-    anchors.options.visible = 'always';
-    anchors.add('h1');
-    opacity = document.querySelector('.anchorjs-link').style.opacity;
-    expect(opacity).toEqual('1');
-  });
-
-  it('should apply a provided class, when that option is set.', function() {
-    var anchorLink;
-    anchors.options.class = 'test-class';
-    anchors.add('h1');
-    anchorLink = document.querySelector('h1 > .test-class');
-    expect(anchorLink).not.toBe(null);
-  });
-
-  it('preserves unicode characters when making text URL-friendly ', function() {
-    var text1Before = 'Заголовок, содержащий 29 не-ASCII символов',
-        text1After = 'заголовок-содержащий-29-не-ascii-символов',
-        text2Before = '船や航海を連想させるものとして、シンボル的に用いられることも多い。',
-        text2After = '船や航海を連想させるものとして、シンボル的に用いられることも多い。',
-        text3Before = 'അടിത്തട്ടിലെ മണ്ണിൽ ആഴ്ന്നിറങ്ങുക, ഭാരത്താൽ താഴ്ന്നു കിടക്കുക, കപ്പലിന്റെ ഗുരുത്വകേന്ദ്രവും',
-        text3After = 'അടിത്തട്ടിലെ-മണ്ണിൽ-ആഴ്ന്നിറങ്ങുക-ഭാരത്താൽ-താഴ്ന്നു-കിടക്കുക-കപ്',
-        text4Before = 'Use ⚓ and 👪 all over the 🌐 can 🔗 inside your webpages.',
-        text4After = 'use-⚓-and-👪-all-over-the-🌐-can-🔗-inside-your-webpages';
-
-    expect(anchors.urlify(text1Before)).toEqual(text1After);
-    expect(anchors.urlify(text2Before)).toEqual(text2After);
-    expect(anchors.urlify(text3Before)).toEqual(text3After);
-    expect(anchors.urlify(text4Before)).toEqual(text4After);
-  });
-
-  it('removes non-url-safe characters when making text URL-friendly', function() {
-    var text1Before = 'one&two three+four$five,six:seven;eight=nine?ten',
-        text2Before = 'one@two"three#four{five}six|seven^eight~nine[ten',
-        text3Before = 'one`two%three!four]five.six/seven(eight)nine*ten\\',
-        after = 'one-two-three-four-five-six-seven-eight-nine-ten';
-
-    expect(anchors.urlify(text1Before)).toEqual(after);
-    expect(anchors.urlify(text2Before)).toEqual(after);
-    expect(anchors.urlify(text3Before)).toEqual(after);
-  });
-
-  it('trims whitespace characters when making text URL-friendly', function() {
-    var text1Before = '\n abc\r',
-        text2Before = 'abc  ',
-        text3Before = 'abc\n ',
-        after = 'abc';
-
-    expect(anchors.urlify(text1Before)).toEqual(after);
-    expect(anchors.urlify(text2Before)).toEqual(after);
-    expect(anchors.urlify(text3Before)).toEqual(after);
-  });
-
-  it('removes apostrophes when making text URL-friendly', function() {
-    var before = 'don\'t',
-        after = 'dont';
-
-    expect(anchors.urlify(before)).toEqual(after);
-  });
-
-  it('truncates IDs via truncate option when making text URL-friendly', function() {
-    var before = 'Today you are you! That is truer than true! There is no one alive who is you-er than you!',
-        trunc1 = 'today-you-are-you-that-is-truer-than-true-there-is-no-one-alive',
-        trunc2 = 'today-you-are-you-that-is-truer-than-true',
-        trunc3 = 'today-you-are-you',
-        trunc4 = 'today-you-are-you-that-is-truer-than-true-there-is-no-one-alive-who-is-you-er-than-you';
-
-    expect(anchors.urlify(before)).toEqual(trunc1);
-    anchors.options.truncate = 41;
-    expect(anchors.urlify(before)).toEqual(trunc2);
-    anchors.options.truncate = 17;
-    expect(anchors.urlify(before)).toEqual(trunc3);
-    anchors.options.truncate = 87;
-    expect(anchors.urlify(before)).toEqual(trunc4);
-
-    // Verify that the final answer remains the same for non-integer numbers or strings.
-    anchors.options.truncate = 87.9999999999;
-    expect(anchors.urlify(before)).toEqual(trunc4);
-    anchors.options.truncate = '87';
-    expect(anchors.urlify(before)).toEqual(trunc4);
-  });
-
-  describe('touch option', function() {
-    beforeEach(function() {
-      anchors.options.visible = 'touch';
+      expect(anchors.urlify(text1Before)).toEqual(text1After);
+      expect(anchors.urlify(text2Before)).toEqual(text2After);
+      expect(anchors.urlify(text3Before)).toEqual(text3After);
+      expect(anchors.urlify(text4Before)).toEqual(text4After);
     });
 
-    it('invokes the `always` behavior for touch devices', function() {
-      var opacity;
-      spyOn(anchors, 'isTouchDevice').and.returnValue(true);
+    it('removes non-url-safe characters', function() {
+      var text1Before = 'one&two three+four$five,six:seven;eight=nine?ten',
+          text2Before = 'one@two"three#four{five}six|seven^eight~nine[ten',
+          text3Before = 'one`two%three!four]five.six/seven(eight)nine*ten\\',
+          after = 'one-two-three-four-five-six-seven-eight-nine-ten';
+
+      expect(anchors.urlify(text1Before)).toEqual(after);
+      expect(anchors.urlify(text2Before)).toEqual(after);
+      expect(anchors.urlify(text3Before)).toEqual(after);
+    });
+
+    it('trims whitespace characters', function() {
+      var text1Before = '\n abc\r',
+          text2Before = 'abc  ',
+          text3Before = 'abc\n ',
+          after = 'abc';
+
+      expect(anchors.urlify(text1Before)).toEqual(after);
+      expect(anchors.urlify(text2Before)).toEqual(after);
+      expect(anchors.urlify(text3Before)).toEqual(after);
+    });
+
+    it('removes apostrophes', function() {
+      var before = 'don\'t',
+          after = 'dont';
+
+      expect(anchors.urlify(before)).toEqual(after);
+    });
+
+    it('truncates IDs using truncate option', function() {
+      var before = 'Today you are you! That is truer than true! There is no one alive who is you-er than you!',
+          trunc1 = 'today-you-are-you-that-is-truer-than-true-there-is-no-one-alive',
+          trunc2 = 'today-you-are-you-that-is-truer-than-true',
+          trunc3 = 'today-you-are-you',
+          trunc4 = 'today-you-are-you-that-is-truer-than-true-there-is-no-one-alive-who-is-you-er-than-you';
+
+      expect(anchors.urlify(before)).toEqual(trunc1);
+      anchors.options.truncate = 41;
+      expect(anchors.urlify(before)).toEqual(trunc2);
+      anchors.options.truncate = 17;
+      expect(anchors.urlify(before)).toEqual(trunc3);
+      anchors.options.truncate = 87;
+      expect(anchors.urlify(before)).toEqual(trunc4);
+
+      // Verify that the final answer remains the same for non-integer numbers or strings.
+      anchors.options.truncate = 87.9999999999;
+      expect(anchors.urlify(before)).toEqual(trunc4);
+      anchors.options.truncate = '87';
+      expect(anchors.urlify(before)).toEqual(trunc4);
+    });
+  });
+
+  describe('icon option', function() {
+    var icon;
+
+    it('allows custom icons to be set, (using octal codes)', function() {
+      anchors.add('h1');
+      icon = document.querySelector('h1 .anchorjs-link').getAttribute('data-anchorjs-icon');
+      expect(icon).toEqual('\ue9cb');
+    });
+
+    it('allows unicode icons to be set', function() {
+      anchors.options.icon = '¶';
+      anchors.add('h1');
+      icon = document.querySelector('h1 .anchorjs-link').getAttribute('data-anchorjs-icon');
+      expect(icon).toEqual('¶');
+    });
+  });
+
+  describe('class option', function() {
+    it('should add the given class to the anchorjs element', function() {
+      var anchorLink;
+      anchors.options.class = 'test-class';
+      anchors.add('h1');
+      anchorLink = document.querySelector('h1 > .anchorjs-link.test-class');
+      expect(anchorLink).not.toBe(null);
+    });
+  });
+
+  describe('placement option', function() {
+    var anchorNode;
+
+    it('`left`, places the anchor to the left of the text.', function() {
+      anchors.options.placement = 'left';
+      anchors.add('h1');
+      anchorNode = document.getElementsByTagName('h1')[0].firstChild;
+      expect(anchorNode.style.position).toEqual('absolute');
+      expect(anchorNode.style.marginLeft).toEqual('-1em');
+    });
+
+    it('`right`, places the anchor to the right of the text.', function() {
+      anchors.options.placement = 'right';
+      anchors.add('h1');
+      anchorNode = document.getElementsByTagName('h1')[0].lastChild;
+      expect(anchorNode.style.position).toEqual('');
+      expect(anchorNode.style.marginLeft).toEqual('');
+    });
+  });
+
+  describe('visibility option', function() {
+    var opacity;
+
+    it('`hover` hides anchor links', function() {
+      anchors.options.visible = 'hover';
+      anchors.add('h1');
+      opacity = document.querySelector('.anchorjs-link').style.opacity;
+      expect(opacity).not.toEqual('1');
+    });
+
+    it('`always` shows anchor links', function() {
+      anchors.options.visible = 'always';
       anchors.add('h1');
       opacity = document.querySelector('.anchorjs-link').style.opacity;
       expect(opacity).toEqual('1');
     });
 
-    it('invokes the `hover` behavior for non-touch devices', function() {
-      var opacity;
-      spyOn(anchors, 'isTouchDevice').and.returnValue(false);
+    it('`touch` invokes the `always` behavior for touch devices', function() {
+      spyOn(anchors, 'isTouchDevice').and.returnValue(true);
+      anchors.options.visible = 'touch';
       anchors.add('h1');
       opacity = document.querySelector('.anchorjs-link').style.opacity;
-      expect(opacity).toEqual('');
+      expect(opacity).toEqual('1');
+    });
+
+    it('`touch` invokes the `hover` behavior for non-touch devices', function() {
+      spyOn(anchors, 'isTouchDevice').and.returnValue(false);
+      anchors.options.visible = 'touch';
+      anchors.add('h1');
+      opacity = document.querySelector('.anchorjs-link').style.opacity;
+      expect(opacity).not.toEqual('1');
     });
   });
 });
+
+/**
+ * A function for the common task of appending an
+ * element to the document body for applying anchors to.
+ * @param {String} tagName  - The string for the tag name you want to create.
+ * @param {String} text     - The text for the textnode inside the tag (optional).
+ * @return {HTMLElement}    - The element you've appended.
+ */
+function appendElementToBody(tagName, text) {
+  'use strict';
+  var el = document.createElement(tagName),
+      textNode;
+
+  if (text) {
+    textNode = document.createTextNode(text);
+    el.appendChild(textNode);
+  }
+  document.body.appendChild(el);
+  return el;
+}
