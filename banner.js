9903680f8c17e5e7ccfbc6526aabf259bb4eb129
XhmikosR <xhmikosr@gmail.com>
2019-09-30 21:17:18 +0300

    Tweak linting.

--- banner.js
+++ banner.js
@@ -1,9 +1,13 @@
+/* eslint-env es6, node */
+
+'use strict';
+
 const fs = require('fs');
 const pkg = require('./package.json');
 const filename = 'anchor.min.js';
 const script = fs.readFileSync(filename);
-const padStart = str => ('0' + str).slice(-2)
-const dateObj = new Date;
+const padStart = str => ('0' + str).slice(-2);
+const dateObj = new Date();
 const date = `${dateObj.getFullYear()}-${padStart(dateObj.getMonth() + 1)}-${padStart(dateObj.getDate())}`;
 const scriptWithBanner = `// @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat
 //
@@ -15,6 +19,6 @@ const scriptWithBanner = `// @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e
 ${script}
 // @license-end`;
 
-if (script.slice(0, 2) != '//') {
+if (script.slice(0, 2) !== '//') {
   fs.writeFileSync(filename, scriptWithBanner);
 }
