5227478336a9cb46758ffae7a5d10ae899ba59cc
XhmikosR <xhmikosr@gmail.com>
2019-10-13 13:54:30 +0300

    Improve banner.js
    Switch to the async fs functions, and output console messages for more info.

--- banner.js
+++ banner.js
@@ -4,21 +4,37 @@
 
 const fs = require('fs');
 const pkg = require('./package.json');
+
 const filename = 'anchor.min.js';
-const script = fs.readFileSync(filename);
 const padStart = str => ('0' + str).slice(-2);
 const dateObj = new Date();
 const date = `${dateObj.getFullYear()}-${padStart(dateObj.getMonth() + 1)}-${padStart(dateObj.getDate())}`;
-const scriptWithBanner = `// @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat
+
+fs.readFile(filename, 'utf8', (err, data) => {
+  if (err) {
+    throw err;
+  }
+
+  const scriptWithBanner = `// @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat
 //
 // AnchorJS - v${pkg.version} - ${date}
 // ${pkg.homepage}
 // Copyright (c) ${dateObj.getFullYear()} Bryan Braun; Licensed ${pkg.license}
 //
 // @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat
-${script}
+${data}
 // @license-end`;
 
-if (script.slice(0, 2) !== '//') {
-  fs.writeFileSync(filename, scriptWithBanner);
-}
+  if (data.startsWith('//')) {
+    console.log(`Banner already present in ${filename}!`);
+  } else {
+    fs.writeFile(filename, scriptWithBanner, 'utf8', (error) => {
+      if (error) {
+        throw error;
+      }
+
+      console.log(`Banner added to ${filename}!`);
+    });
+  }
+});
+
