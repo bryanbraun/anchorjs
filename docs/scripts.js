552e41adf3f6db8400ec7839ae2f649d20020f16
Bryan Braun <bbraun7@gmail.com>
2020-10-16 10:43:50 -0400

    fix: use var instead of let in docs to ensure IE9+ compat

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,7 +1,7 @@
 function slideExampleCode(e) {
   e.preventDefault();
 
-  let slide = e.target.parentElement.nextElementSibling;
+  var slide = e.target.parentElement.nextElementSibling;
   slide.classList.toggle('open');
 
   if(slide.classList.contains('open')) {
@@ -12,20 +12,20 @@ function slideExampleCode(e) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  let toggleButtons = document.getElementsByClassName('example-code-link'); // get the buttons
+  var toggleButtons = document.getElementsByClassName('example-code-link'); // get the buttons
 
-  for(let i = 0; i < toggleButtons.length; i++) {
+  for(var i = 0; i < toggleButtons.length; i++) {
     toggleButtons[i].addEventListener("click", slideExampleCode); // add the event to each button
   }
 
   // Dynamically add PrismJS class for syntax highlight
-  let jsCodes = document.querySelectorAll('pre[class*="js"] code');
-  for(let i = 0; i < jsCodes.length; i++) {
+  var jsCodes = document.querySelectorAll('pre[class*="js"] code');
+  for(var i = 0; i < jsCodes.length; i++) {
     jsCodes[i].classList.add('language-javascript');
   }
 
-  let cssCodes = document.querySelectorAll('pre.css code');
-  for(let i = 0; i < cssCodes.length; i++) {
+  var cssCodes = document.querySelectorAll('pre.css code');
+  for(var i = 0; i < cssCodes.length; i++) {
     cssCodes[i].classList.add('language-css');
   }
 });
