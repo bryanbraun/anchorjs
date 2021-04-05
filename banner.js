b3a0779ca57dd1253d219836d54fa17c45d7e4f8
XhmikosR <xhmikosr@gmail.com>
2021-04-05 08:45:52 +0300

    banner.js: minor tweaks

--- banner.js
+++ banner.js
@@ -3,12 +3,13 @@
 'use strict';
 
 const fs = require('fs');
-const pkg = require('./package.json');
+const { version, homepage, license } = require('./package.json');
 
 const filename = 'anchor.min.js';
 const padStart = str => String(str).padStart(2, '0');
 const dateObj = new Date();
-const date = `${dateObj.getFullYear()}-${padStart(dateObj.getMonth() + 1)}-${padStart(dateObj.getDate())}`;
+const year = dateObj.getFullYear();
+const fullDate = `${year}-${padStart(dateObj.getMonth() + 1)}-${padStart(dateObj.getDate())}`;
 
 fs.readFile(filename, 'utf8', (err, data) => {
   if (err) {
@@ -17,9 +18,9 @@ fs.readFile(filename, 'utf8', (err, data) => {
 
   const scriptWithBanner = `// @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat
 //
-// AnchorJS - v${pkg.version} - ${date}
-// ${pkg.homepage}
-// Copyright (c) ${dateObj.getFullYear()} Bryan Braun; Licensed ${pkg.license}
+// AnchorJS - v${version} - ${fullDate}
+// ${homepage}
+// Copyright (c) ${year} Bryan Braun; Licensed ${license}
 //
 // @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat
 ${data}
@@ -37,4 +38,3 @@ ${data}
     });
   }
 });
-
